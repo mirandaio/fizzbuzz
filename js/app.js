6a9d321f96b8513d928bf70be113b5bd076af7e7
Alfredo Miranda <alfredo@miranda.io>
2014-09-17 21:50:03 -0500

    Now user can enter number that represents end of fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 $(function() {
-  for(var i = 1; i <= 100; i++) {
-    if(i % 15 == 0) {
-      $('ul').append('<li>fizz buzz</li>');
-    } else if(i % 3 == 0) {
-      $('ul').append('<li>fizz</li>');
-    } else if(i % 5 == 0) {
-      $('ul').append('<li>buzz</li>');
-    } else {
-      $('ul').append('<li>' + i + '</li>');
+  $('input').keypress(function(event) {
+    if(event.which == 13) {
+      var val = +$(this).val();
+      if(isNaN(val)) {
+        alert("You did not supply a number!");
+      } else {
+        $('li').remove();
+        fizzbuzz(val);
+      }
+    }
+  });
+
+  function fizzbuzz(num) {
+    for(var i = 1; i <= num; i++) {
+      if(i % 15 == 0) {
+        $('ul').append('<li>fizz buzz</li>');
+      } else if(i % 3 == 0) {
+        $('ul').append('<li>fizz</li>');
+      } else if(i % 5 == 0) {
+        $('ul').append('<li>buzz</li>');
+      } else {
+        $('ul').append('<li>' + i + '</li>');
+      }
     }
   }
 });
